d0f7ffddb654e8cc335a63034d412a020a9ce5cd
Mars Kitten <57377187+MarsKittenPro@users.noreply.github.com>
2023-05-17 14:13:17 +1000

    fax

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.get("/llama", async (req,res) => {
     res.redirect("https://raw.githubusercontent.com/Zycrasion/LaaS/main/".concat(filename));
 });
 
-app.get("/llama_url", 
-async (req,res) => {
+app.get("/llama_url", async (req,res) => {
     let index = await fs.readFile(path.join(process.cwd(), "llama/index"), {encoding:"utf-8"});
     index = index.split("\n");
     
@@ -22,11 +21,19 @@ async (req,res) => {
     let pathToFile = "https://raw.githubusercontent.com/Zycrasion/LaaS/main/".concat(filename);
 
     res.send(pathToFile)
-}
-)
+})
+
+app.get("/llama_fax", async (req,res) => {
+    let facts = await fs.readFile(path.join(process.cwd(), "llama_fax"), {encoding:"utf-8"});
+    facts = facts.split("\n");
+
+    let fact = facts[Math.floor(Math.random() * facts.length)];
+
+    res.send(fact)
+})
 
 app.get("/", (req,res) => {
-    res.send("/llama for llamas")
+    res.send("/llama for llamas\n/llama_url for picture url\n/llama_fax for random llama fax")
 })
 
 app.listen(5000, ()=>{console.log("READY")});
