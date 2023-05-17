068d2a5a8b66676d0ff55f21bb25dcc470f665ec
Mars Kitten <57377187+MarsKittenPro@users.noreply.github.com>
2023-05-17 13:47:30 +1000

    new path

--- index.js
+++ index.js
@@ -13,8 +13,20 @@ app.get("/llama", async (req,res) => {
     res.redirect("https://raw.githubusercontent.com/Zycrasion/LaaS/main/".concat(filename));
 });
 
+app.get("/llama_url", 
+async (req,res) => {
+    let index = await fs.readFile(path.join(process.cwd(), "llama/index"), {encoding:"utf-8"});
+    index = index.split("\n");
+    
+    let filename = "llama/".concat(index[Math.floor(Math.random() * index.length)]);
+    let pathToFile = "https://raw.githubusercontent.com/Zycrasion/LaaS/main/".concat(filename);
+
+    res.send(pathToFile)
+}
+)
+
 app.get("/", (req,res) => {
-    res.redirect("/llama")
+    res.send("/llama for llamas")
 })
 
 app.listen(5000, ()=>{console.log("READY")});
